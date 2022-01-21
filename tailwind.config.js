43882a6035fcafd090c858691e5ee4796035427a
doubled3264 <dede.doubled3264@gmail.com>
2022-01-21 08:35:53 +0700

    edit tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-   purge: [],
    presets: [],
    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-   darkMode: false, // or 'media' or 'class'
    theme: {
       screens: {
          sm: "640px",
@@ -19,7 +17,7 @@ module.exports = {
 
          black: colors.black,
          white: colors.white,
-         gray: colors.coolGray,
+         gray: colors.gray,
          red: colors.red,
          yellow: colors.amber,
          green: colors.emerald,
